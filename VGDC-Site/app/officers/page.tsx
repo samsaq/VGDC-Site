a3a19f6c28c6dea01630387fb5783bda3c6f1f1d
Alvin Ton <157676125+breadyaboi@users.noreply.github.com>
2025-03-07 01:36:49 -0700

    Update format of officer page

--- VGDC-Site/app/officers/page.tsx
+++ VGDC-Site/app/officers/page.tsx
@@ -7,18 +7,19 @@ export default function officers()
              <Navbar/>
              <div>{/*Non navbar container*/}
                 <div>
-                    <div className="w-3/5 overflow-hidden">{/*Image container slider container*/}
-                        <img
-                        src="filler_image.jpg"
-                        className="w-80 h-80 object-cover rounded-lg"
-                        />
-                    </div>
+                <div className="w-3/5 overflow-hidden flex justify-center items-center">
+                    <img
+                    src="filler_image.jpg"
+                    className="w-full max-w-[500px] aspect-square object-cover rounded-lg"
+                    />
+                </div>
+
                     <div className="w-1/10 flex justify-center py-10">{/*select line container*/}
                         <div className="h-[4px] w-[150px] bg-orange-400"></div>
                     </div>
                     <div className="w-3/10 font-outfit flex">{/*triple split container*/}
                         <div className="w-1/3 text-orange-400">{/*caption header */}
-                            <h1 className="text-5xl">Meet our club officers</h1>
+                            <strong className="text-6xl">Meet our club officers</strong>
                         </div>
                         <div className="w-1/3">
                             <div>
@@ -28,11 +29,11 @@ export default function officers()
                             Lorem ipsum dolor sit amet consectetur adipiscing elit volutpat gravida malesuada quam commodo id integer nam.
                             </h1>
                         </div>
-                        <div className="w-1/3 flex justify-start space-x-2">
-                            <button className="bg-orange-400 rounded-full w-16 h-16 flex items-center justify-center text-white text-2xl">
+                        <div className="w-1/3 flex justify-center px-2 space-x-4">
+                            <button className="bg-orange-400 rounded-full w-20 h-20 flex items-center justify-center text-white text-4xl">
                                 <span>&lt;</span>
                             </button>
-                            <button className="bg-orange-400 rounded-full w-16 h-16 flex items-center justify-center text-white text-2xl">
+                            <button className="bg-orange-400 rounded-full w-20 h-20 flex items-center justify-center text-white text-4xl">
                                 <span className="text-light"> &gt; </span>
                             </button>
                         </div>
