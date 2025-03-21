adc765c30d4d342faccfaf63321b3a0005adb80b
samsaq <sameereza@gmail.com>
2025-03-21 11:24:47 -0700

    fixed landing page layout centering on ultrawide screens

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
           }}
         >
           <div className="relative flex h-screen flex-col">
-            <main className="container w-full flex-grow">{children}</main>
+            <main className="container min-w-full flex-grow">{children}</main>
           </div>
         </Providers>
       </body>
