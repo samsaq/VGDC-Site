095a8d3406b1b11274578bcac7647bccbe1e20b1
Alvin Ton <157676125+breadyaboi@users.noreply.github.com>
2025-04-04 09:55:38 -0700

    access to env

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,3 +6,7 @@ export async function getTeamsData() {
   const teams = getDocuments("teams", ["title", "content", "coverImage"]);
   return teams;
 }
+export async function getAboutData(){
+  const about = getDocuments("about", ["title", "content", "coverImage"]);
+  return about;
+}
