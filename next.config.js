e9cd3e92f7ae7156191524941ceabf9c9e8e9b6e
samsaq <sameereza@gmail.com>
2025-05-03 14:46:20 -0700

    config fix for build process

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        outputFileTracingIncludes: {
+            '**/*': ['./outstatic/**/*']
+        }
+    }
+};
 
 module.exports = nextConfig;
